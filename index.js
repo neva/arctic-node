3bf27b24b9506d7636d3d1e12977aa040ace3a3e
paul-hanneforth <paul.hanneforth.o@gmail.de>
2020-07-03 11:58:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = (appToken) => {
+
+    const router = require("express").Router();
+    const cookieParser = require("cookie-parser");
+    const axios = require("axios");
+    
+    const serverAddress = "http://localhost:3000"
+    
+    const getUserData = async (userAuthToken, appToken) => {
+        const response = await axios.post(serverAddress + "/app/user", {
+            userAuthToken,
+            appToken
+        })
+        return response.data;
+    }
+    
+    router.use(cookieParser())
+    
+    router.use(async (req, res, next) => {
+    
+        const queryAuthToken = req.query.authToken;
+        const cookieAuthToken = req.cookies.authToken;
+    
+        if(cookieAuthToken == undefined && queryAuthToken) {
+            res.cookie("authToken", queryAuthToken);
+        }
+        if(queryAuthToken) {
+            res.cookie("authToken", queryAuthToken);
+            req.authToken = queryAuthToken;
+        } else if(cookieAuthToken) {
+            req.authToken = req.cookies.authToken;
+        } else {
+            req.authToken = null;
+        }
+    
+        const data = await getUserData(req.authToken, appToken);
+        req.user = {
+            email: data.data.email,
+            name: data.data.name,
+            authToken: req.authToken
+        }
+    
+        next();
+    
+    })
+
+    return router;
+    
+};
